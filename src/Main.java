9f5e59a455160419b1b8759bdd890d214e820fce
Marc <121861629+GidJoe@users.noreply.github.com>
2023-01-23 12:55:57 +0100

    RLS

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        //char[][] laby = new char[12][10];
+
+
+            char[][] laby ={
+                    {'#', '#', '#', '#', '#', 'Z', '#'},
+                    {'#', ' ', ' ', ' ', '#', ' ', '#'},
+                    {'#', ' ', '#', '#', '#', ' ', '#'},
+                    {'#', ' ', '#', ' ', ' ', ' ', '#'},
+                    {'#', ' ', ' ', ' ', '#', '#', '#'},
+                    {'#', ' ', '#', ' ', '#', ' ', '#'},
+                    {'#', ' ', '#', ' ', '#', ' ', '#'},
+                    {'#', ' ', '#', ' ', ' ', ' ', '#'},
+                    {'#', ' ', '#', ' ', '#', ' ', '#'},
+                    {'#', ' ', '#', ' ', '#', ' ', '#'},
+                    {'#', 'S', '#', '#', '#', '#', '#'},
+            };
+
+        Point start = null;
+        for (int zeile = 0; zeile < laby.length; zeile++) {
+            for (int spalte = 0; spalte < laby[zeile].length; spalte++) {
+                if (laby[spalte][zeile] == 'S') {
+                    start = new Point(zeile, spalte);
+                }
+            }
+        }
     }
 }
