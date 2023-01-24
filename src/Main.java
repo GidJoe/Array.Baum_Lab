be5e26e4c8c3c21e4fe86b4771c62f4e445de5fd
GidJoe <winterm@bfwob.de>
2023-01-24 16:17:53 +0100

    RLS

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
@@ -24,10 +26,45 @@ public class Main {
         Point start = null;
         for (int zeile = 0; zeile < laby.length; zeile++) {
             for (int spalte = 0; spalte < laby[zeile].length; spalte++) {
-                if (laby[spalte][zeile] == 'S') {
+                if (laby[zeile][spalte] == 'S') {
                     start = new Point(zeile, spalte);
                 }
             }
+         }
+        Point ziel = null;
+
+        System.out.println(start);
+        // point.x = Zeile
+
+        int[][] distanz = new int[laby.length][laby[0].length];
+        for(int[]  sub: distanz) {
+            Arrays.fill(sub, -1);
         }
+        distanz[start.x][start.y] = 0;
+
+        ArrayList<Point> suche = new ArrayList<>();
+        suche.add(start);
+
+        Point standfeld;
+        while (!suche.isEmpty()) {
+            standfeld = suche.get(0);
+            // guck nach oben
+            if (standfeld.x > 0) {  // wir stehen nicht oben am Rand
+                if (laby[standfeld.x-1][standfeld.y] != '#'
+                    && distanz[standfeld.x-1][standfeld.y] == -1) {
+                    distanz[standfeld.x-1][standfeld.y] = distanz[standfeld.x][standfeld.y] + 1;
+                    suche.add(new Point(standfeld.x-1,standfeld.y));
+                }
+            }
+
+            // guck nach unten
+
+            // guck nach links
+
+            // guck nach rechts
+
+            suche.remove(standfeld);
+        }
+
     }
 }
